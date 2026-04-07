29adcdf1ab8d3b079478810e2aa0fed90c88af57
Rhy <sushimachiatto@gmail.com>
2026-04-07 11:47:20 -0600

    Update project

--- src/cs2050project.java
+++ src/cs2050project.java
@@ -3,10 +3,11 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class cs2050project 
-{
-    public static void main(String[] args)
-    {
+/**
+ * 
+ */
+public class cs2050project {
+    public static void main(String[] args) {
         // Create hanger in main
     }
 }
@@ -14,157 +15,172 @@ public class cs2050project
 /**
  * 
  */
-class Hangar
-{
+class Hangar {
+
     ArrayList<Drone> dronesList; // We will use Arraylists
 
     /**
-     * Iterates through ArrayList in Hangar Class and increments counter if manufacturer name matches input string.
+     * Iterates through ArrayList in Hangar Class and increments counter if
+     * manufacturer name matches input string.
+     * 
      * @param manufacturer
      * @return count
      */
-    public int getCountByManufacturer(String manufacturer)
-    {
+    public int getCountByManufacturer(String manufacturer) {
         int count = 0;
 
-        for (int i = 0; i < dronesList.size();i++) // for loop iterating for ArrayList
+        for (int i = 0; i < dronesList.size(); i++) // for loop iterating for ArrayList
         {
             if (dronesList.get(i).getManufacturer() == manufacturer) // Can use Instanceof
             {
                 count++;
             }
         }
-        
+
         return count;
     }
 
-    public void generateReportSortedByManufacturingYear(ArrayList<Drone>dronesList)
-    {
+    public void generateReportSortedByManufacturingYear(ArrayList<Drone> dronesList) {
 
     }
 
-    public void generateReportSortedByPayloadCapacity(ArrayList<Drone>dronesList)
-    {
-        /** TODO:
-         *  1. Sort list into new Array by Payload Capacity 
-         *  2. Print the output of the list (either during runtime or after the sort)
+    public void generateReportSortedByPayloadCapacity(ArrayList<Drone> dronesList) {
+        /**
+         * TODO:
+         * 1. Sort list into new Array by Payload Capacity
+         * 2. Print the output of the list (either during runtime or after the sort)
+         * 3.
          */
 
     }
 
-    public void displayHangarInventory()
-    {
-        //Display formatted printout of inventory
+    public void displayHangarInventory() {
+        // Display formatted printout of inventory
     }
 
-    public void showMenu()
-    {
+    public void showMenu() {
         // TODO: Null check -> while isvalid, Use Switch Case for the rest
+        // Refer to L16
         Scanner input = new Scanner(System.in);
-        boolean bool = true;
+        boolean runtime = true;
 
         String menu[] = {
-                         "1. Load Drones from CSV",
-                         "2. Display Hangar Inventory", 
-                         "3. Search Drones (Manufacturer & Type)", 
-                         "4. View Inventory Sorted by Payload (Manual Sort)",
-                         "5. View Inventory Sorted by Year (Manual Sort)",
-                         "6. Count Drones by Manufacturer",
-                         "7. Exit"
-                        };
+                "1. Load Drones from CSV",
+                "2. Display Hangar Inventory",
+                "3. Search Drones (Manufacturer & Type)",
+                "4. View Inventory Sorted by Payload (Manual Sort)",
+                "5. View Inventory Sorted by Year (Manual Sort)",
+                "6. Count Drones by Manufacturer",
+                "7. Exit"
+        };
 
-        while (bool == true)
-        {
+        // test ex look at lab16
+        // Use
+        while (runtime) {
             System.out.println("=== Drone Hangar Menu ===");
 
-            for (int i = 0; i < menu.length; i++)
-            {
+            for (int i = 0; i < menu.length; i++) {
                 System.out.println(menu[i]);
             }
 
-            System.out.println("Enter your Choice(1-7): ");
+            System.out.println("Enter your Choice: ");
             int output = input.nextInt();
 
-            // use Switch case For choices AFTER checking
-            switch (output)
-            {
-                case 1:
+            if (input.hasNextInt()) {
+                int response = input.nextInt();
+                input.nextLine();
 
-                case 2:
+                switch (response) {
+                    case 1:
+                        // Load drones from CSV
+                    case 2:
+                        // Display hangar inventory
+                    case 3:
+                        // Search Drones (Manufacturer & Type)
+                    case 4:
+                        // View Inventory by Payload
+                    case 5:
+                        // View Inventory by Year
+                    case 6:
+                        // Count drone by manufacturer
+                    case 7:
+                        // Exit
 
-                default:
+                    default:
+                        System.out.println("Invalid selection. Please pick from 1-" + menu.length);
+                }
+
+            } else {
+                System.out.println("Invalid input, please insert a valid number.");
+                input.nextLine();
             }
 
         }
 
     }
 
-    public boolean addDrone(Drone drone)
-    {
+    /**
+     * Adds a drone to the hangar. Used in CSVreader, has null check to allow other
+     * ways of adding a drone
+     * 
+     * @param drone
+     * @return
+     */
+    public boolean addDrone(Drone drone) {
         // redudancy null check
-        if (drone != null)
-        {
+        if (drone != null) {
             this.dronesList.add(drone);
             return true;
-        }
-        else
-        {
+        } else {
             return false;
         }
     }
 
     /**
-     * Read From CSV uses an input CSV file in directory to load and then parse to a readable format.
+     * Read From CSV uses an input CSV file in directory to load and then parse to a
+     * readable format.
+     * 
      * @param hangar
      * @param fileName
      */
-    public static void readFromCSV(Hangar hangar, String fileName)
-    {
+    public static void readFromCSV(Hangar hangar, String fileName) {
         int totalLinesRead = 0;
 
-        if (hangar == null)
-        {
+        if (hangar == null) {
             System.out.println("Error. Hangar not found.");
             return;
         }
 
-        // try catch 
-        try(Scanner fileScan = new Scanner(new File(fileName)))
-        {
+        // try catch
+        try (Scanner fileScan = new Scanner(new File(fileName))) {
             // Continuous checking to iterate through entire file
-            while (fileScan.hasNextLine())
-            {
+            while (fileScan.hasNextLine()) {
                 String line = fileScan.nextLine();
 
                 // Increment totalLinesRead to keep track of lines and add drones
                 totalLinesRead = totalLinesRead + 1;
                 boolean shouldProcess = true;
-                
-                if (line == null || line.trim().isEmpty())
-                {
+
+                if (line == null || line.trim().isEmpty()) {
                     shouldProcess = false;
                 }
-                
-                // If conditionals above are true then we can proceed and prepare to parse and add a drone
-                if(shouldProcess == true)
-                {
-                    // Send to helper method to parse the drone line into a new drone
-                    Drone newDrone = parseDroneLine(line,totalLinesRead);
 
-                    if (newDrone != null)
-                    {
+                // If conditionals above are true then we can proceed and prepare to parse and
+                // add a drone
+                if (shouldProcess == true) {
+                    // Send to helper method to parse the drone line into a new drone
+                    Drone newDrone = parseDroneLine(line, totalLinesRead);
+
+                    if (newDrone != null) {
                         boolean added = hangar.addDrone(newDrone);
-                        if (added == false)
-                        {
+                        if (added == false) {
                             System.out.println("Error. Unable to add Drone.");
                         }
 
                     }
                 }
             }
-        }
-        catch (FileNotFoundException ex)
-        {
+        } catch (FileNotFoundException ex) {
             System.out.println("Error. Could not open file: " + fileName);
             return;
         }
@@ -177,29 +193,30 @@ class Hangar
      * @param LineNumber
      * @return
      */
-    private static Drone parseDroneLine(String line, int LineNumber)
-    {
-        /**TODO: 
+    private static Drone parseDroneLine(String line, int LineNumber) {
+        /**
+         * TODO:
          * Check if the line is empty (or null)
          * Trim fields
-         * Validate text fields (Type of drone, ManufacturerName, ManufacturerYear, weight limit in KG)
+         * Validate text fields (Type of drone, ManufacturerName, ManufacturerYear,
+         * weight limit in KG)
          * Use Try-Catch for year and payloadKG checking and if statements for year
-        */
-        if (line.trim().isEmpty() || line == null)
-        {
+         * Pretty iffy- find lecture for the lab
+         */
+        if (line.trim().isEmpty() || line == null) {
             return null;
         }
 
         String[] fields = line.split(","); // CSV -> Comma Seperated Values
-        
-        // Utilize array of our four inputs that we need: If P or S, Name, Year, Weight Limit and store them into variables
-        String droneType = fields[0].trim(); 
+
+        // Utilize array of our four inputs that we need: If P or S, Name, Year, Weight
+        // Limit and store them into variables
+        String droneType = fields[0].trim();
         String manufacturerName = fields[1].trim();
         String manufacturerYear = fields[2].trim();
         String payloadKg = fields[3].trim();
 
-        if (fields.length != 4)
-        {
+        if (fields.length != 4) {
             System.out.println("Error. Invalid number of columns. Expected 4, recieved " + fields.length);
             return null;
         }
@@ -208,39 +225,34 @@ class Hangar
     }
 }
 
-/** 
+/**
  * Abstract Drone class used for creation of Priority and Standard Drones.
  */
-abstract class Drone
-{
+abstract class Drone {
     private String manufacturerName;
     private int manufacturerYear;
     private double payloadKg;
 
-    public Drone (String manufacturerName, int manufacturerYear,double payloadKg)
-    {
+    public Drone(String manufacturerName, int manufacturerYear, double payloadKg) {
         this.manufacturerName = manufacturerName;
         this.manufacturerYear = manufacturerYear;
         this.payloadKg = payloadKg;
     }
 
-    public String getManufacturer()
-    {
+    public String getManufacturer() {
         return this.manufacturerName;
     }
 
-    public int getManufacturerYear()
-    {
+    public int getManufacturerYear() {
         return this.manufacturerYear;
     }
 
-    public double getPayloadKg()
-    {
+    public double getPayloadKg() {
         return this.payloadKg;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         String toString = "";
         return toString;
     }
@@ -249,10 +261,8 @@ abstract class Drone
 /**
  * Priority Drone
  */
-class PriorityDrone extends Drone
-{
-    public PriorityDrone(String manufacturerName, int manufacturerYear,double payloadKg)
-    {
+class PriorityDrone extends Drone {
+    public PriorityDrone(String manufacturerName, int manufacturerYear, double payloadKg) {
         super(manufacturerName, manufacturerYear, payloadKg);
     }
 
@@ -261,10 +271,8 @@ class PriorityDrone extends Drone
 /**
  * Standard Drone
  */
-class StandardDrone extends Drone
-{
-    public StandardDrone(String manufacturerName, int manufacturerYear, double payloadKg)
-    {
+class StandardDrone extends Drone {
+    public StandardDrone(String manufacturerName, int manufacturerYear, double payloadKg) {
         super(manufacturerName, manufacturerYear, payloadKg);
     }
 
